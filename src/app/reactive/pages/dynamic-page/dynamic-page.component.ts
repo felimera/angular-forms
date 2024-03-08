1b2e80e08d919493b9c01c8fee4c20b801672b25
andres.mera <feli041089@gmail.com>
2024-03-08 13:23:00 -0500

    developer : Formularios dinámicos.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGame: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGame') as FormArray;
+  }
+  onSumit(): void {
+    if (!this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log('this.myForm.value ', this.myForm.value);
+    this.myForm.reset();
+  }
 }
