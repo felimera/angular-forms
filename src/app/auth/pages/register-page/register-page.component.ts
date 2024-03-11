70b3b71c7b6a912b18f3514c4ab3a0dec213689a
andres.mera <feli041089@gmail.com>
2024-03-11 08:09:09 -0500

    developer : Formulario de registro.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  isValidField(field: string): void {
+    // TODO: obtener validator desde un servicio.
+  }
+  onSumit(): void {
+    this.myForm.markAllAsTouched();
+
+    console.log('this.myForm.value ', this.myForm.value);
+  }
 }
