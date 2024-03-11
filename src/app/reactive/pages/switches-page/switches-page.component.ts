4531d5c2b886be0544a60dc2bdedbaac68478a22
andres.mera <feli041089@gmail.com>
2024-03-11 07:48:39 -0500

    developer : Solucion a la tarea.

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     gender: ['M', Validators.required],
@@ -13,28 +13,20 @@ export class SwitchesPageComponent {
     termsAndCondition: [false, Validators.requiredTrue],
   });
 
+  public person = {
+    gender: 'F',
+    wantNotifications: true
+  }
+
   constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
 
   isValidField(field: string): boolean | null {
     return this.myForm.controls[field].errors
       && this.myForm.controls[field].touched;
-  }
-
-  getFieldError(field: string): string | null {
-    if (!this.myForm.controls[field]) return null;
-
-    const errors = this.myForm.controls[field].errors || {};
-
-    for (const key of Object.keys(errors)) {
-      switch (key) {
-        case 'required':
-          return 'Este campo es requerido';
-
-        case 'minlength':
-          return `Minimo ${errors['minlength'].requiredLength} caracters.`;
-      }
-    }
-    return null;
   }
 
   onSaved(): void {
@@ -42,5 +34,11 @@ export class SwitchesPageComponent {
       this.myForm.markAllAsTouched();
       return;
     }
+
+    const { termsAndCondition, ...newPerson } = this.myForm.value;
+    this.person = this.myForm.value;
+    console.log('this.myForm.value', this.myForm.value);
+    console.log('this.person', this.person);
+    console.log('newPerson', newPerson);
   }
 }
